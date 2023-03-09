0fde40b13a856d8f401d5c54a1046d3fe4d5a5b8
Ale018 <106705670+Ale018@users.noreply.github.com>
2023-03-09 15:51:07 -0600

    Backend Version 1

---
+++ models/productModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ProductoEsquema = new mongoose.Schema({
+ id: Number,
+ name: { type: String,required:true,},
+ description: {type: String,},
+ price: { type: Number, min:0,},
+ stock: Number,
+ images:[String],
+});
+
+module.exports = mongoose.model("Producto", ProductoEsquema);
